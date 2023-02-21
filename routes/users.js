2ef74f78283e16d3fa04ecfbafc19b01c6866ddd
kmfoysal <contact.kmfoysal@gmail.com>
2023-02-21 21:25:28 +0600

    create hotel and room api

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,36 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/usersController.js";
-import { verifyAdmin, verifyUser } from "../middlewares/verifyAuth.js";
+import { verifyAuth } from "../middlewares/verifyAuth.js";
 
 const router = express.Router();
 
-// router.get("/checkauthentication", verifyToken, (req,res,next)=>{
-//   res.send("hello user, you are logged in")
-// })
+router.get("/checkauthentication", verifyAuth, (req, res, next) => {
+    res.send("hello user, you are logged in");
+});
 
-// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
-//   res.send("hello user, you are logged in and you can delete your account")
-// })
+router.get("/checkuser/:id", verifyAuth, (req,res,next)=>{
+  
+        res.send("hello user, you are logged in and you can delete your account");
+   
+})
 
-// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
-//   res.send("hello admin, you are logged in and you can delete all accounts")
-// })
-
-//UPDATE
-router.put("/:id", verifyUser, updateUser);
-
-//DELETE
-router.delete("/:id", verifyUser, deleteUser);
-
-//GET
-router.get("/:id", verifyUser, getUser);
+router.get("/checkadmin/:id", verifyAuth, (req, res, next) => {
+    res.send("hello admin, you are logged in and you can delete all accounts");
+});
 
 //GET ALL
-router.get("/", verifyAdmin, getUsers);
+router.get("/", verifyAuth, getUsers);
+
+//UPDATE
+router.put("/:id", verifyAuth, updateUser);
+
+//DELETE
+router.delete("/:id", verifyAuth, deleteUser);
+
+//GET
+router.get("/:id", verifyAuth, getUser);
+
+//GET ALL
+// router.get("/", verifyAdmin, getUsers);
 
 export default router;
